b751c323c426c93e0c16aad47dcef7e1550c27a8
della22 <della.harmi22@gmail.com>
2021-10-15 21:28:53 +0700

    Tambah file postest 2

---
+++ PERT2/POSTEST/pwd2.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 15 Okt 2021 pada 16.23
+-- Versi server: 10.4.18-MariaDB
+-- Versi PHP: 7.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pwd2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `Tanggal_lahir` date DEFAULT NULL,
+  `Jumlah_transaksi` int(11) DEFAULT NULL,
+  `Total_transaksi` int(20) DEFAULT NULL,
+  `Alamat` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `transaksi`
+--
+
+INSERT INTO `transaksi` (`id`, `username`, `password`, `Tanggal_lahir`, `Jumlah_transaksi`, `Total_transaksi`, `Alamat`) VALUES
+(1, 'Aris', 'zxcvbnm', '1987-09-09', 6, 10000, 'Jl. Ahmad Yani'),
+(2, 'Budi', 'asdfghj', '1994-01-01', 0, 0, 'Jl. Depati Amir'),
+(3, 'Charlie', 'lkqwert', '1965-12-06', 24, 312150, 'Jl. Yos Sudarso'),
+(4, 'Daniel', 'poiuytr', '1980-04-24', 3, 0, 'Jl. Soekarno');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
