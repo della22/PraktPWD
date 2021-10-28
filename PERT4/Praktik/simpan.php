801274a2a19eaf7bb006d3720842e4854f5aeda6
della22 <della.harmi22@gmail.com>
2021-10-28 20:32:45 +0700

    Tambah praktik pert 4

---
+++ PERT4/Praktik/simpan.php
@@ -0,0 +1,52 @@
+<?php
+// Create database connection using config file
+include_once("koneksi.php");
+// Fetch all users data from database
+$result = mysqli_query($con, "SELECT * FROM data ");
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>CRUD PWD 4!</title>
+  </head>
+  <body>
+    <div class="container">
+    <h1>List Data</h1>
+
+    <table class="table table-bordered">
+    <thead>
+        <tr>
+        <th>Nama</th> 
+        <th>Email</th> 
+        <th>Website</th> 
+        <th>Comment</th> 
+        <th>Gender</th> 
+        </tr>
+    </thead>
+
+    <tbody>
+
+    <?php 
+    while($user_data = mysqli_fetch_array($result)) { ?>
+        <tr>
+        <td><?= $user_data['nama']; ?></td>
+        <td><?= $user_data['email']; ?></td>
+        <td><?= $user_data['website']; ?></td>
+        <td><?= $user_data['comment']; ?></td>
+        <td><?= $user_data['gender']; ?></td>
+    <?php } ?>
+    <tbody>
+    </table>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+  </body>
+</html>
