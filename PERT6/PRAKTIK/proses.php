ca68be882f970e125cb3a05477c862992d3d0253
della22 <della.harmi22@gmail.com>
2021-11-25 19:06:10 +0700

    Tambah file praktik pert 6

---
+++ PERT6/PRAKTIK/proses.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+include "koneksi.php";
+$user = $_POST['id_user'];
+$pass = md5($_POST['password']);
+$sql = "SELECT * FROM users WHERE id_user='$user' AND password='$pass'";
+if($_POST["captcha_code"] == $_SESSION["captcha_code"]){
+    $login = mysqli_query($con, $sql);
+    $ketemu = mysqli_num_rows($login);
+    $r = mysqli_fetch_array($login);
+    if ($ketemu > 0){
+        $_SESSION['id'] = $r['id_user'];
+        $_SESSION['mail'] = $r['email'];
+        $_SESSION['nama'] = $r['nama_lengkap'];
+        header("location: beranda.php");
+    }else{
+        echo "<center>Login gagal username & password tidak benar<br>";
+        echo "<a href=index.php><b>ULANGI LAGI</b></a></center>";
+    }
+    mysqli_close($con);
+}else{
+    echo "<center>Login gagal! Captcha tidak sesuai<br>";
+    echo "<a href=index.php><b>ULANGI LAGI</b></a></center>";
+}
